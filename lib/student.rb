9bd81214d8a2a40047bb4a12e9e0de73e9663c91
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-14 15:01:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    student = Student.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    Student.new.tap do
+      student.id = row[0]
+      student.name = row[1]
+      student.grade = row[2]
+    end
   end
 
   def self.all
