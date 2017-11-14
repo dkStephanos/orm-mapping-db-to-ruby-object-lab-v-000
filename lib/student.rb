585f8d68646e4518054e222aea5377ff6e274c7c
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-14 15:15:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,7 +94,7 @@ class Student
       LIMIT ?
     SQL
     #binding.pry
-    DB[:conn].execute(sql, "10th", num_students.to_s).collect do |row|
+    DB[:conn].execute(sql, "10th", num_students).collect do |row|
       Student.new_from_db(row)
     end
   end
